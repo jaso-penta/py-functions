2543f40b40a2c692f9b43ad7c14edc4a7612b4d6
Jašo <andrija.pericic@gmail.com>
2025-09-23 21:06:02 +0200

    Create py_func_03.py

---
+++ py_func_03.py
@@ -0,0 +1,16 @@
+import typing
+
+def podijeli(a : int, b : int) -> None:
+    print(a / b)
+
+
+x = 4
+y = 5
+
+
+podijeli(5, 15)
+podijeli(b=15, a=5)
+podijeli(15, 5)
+podijeli(x, y)
+podijeli(y, x)
+podijeli(b=y, a=x)
