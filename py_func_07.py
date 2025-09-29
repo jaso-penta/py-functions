da103ad31c8e4481bde98427ffad99f82b18d615
Jašo <andrija.pericic@gmail.com>
2025-09-29 18:33:50 +0200

    Create py_func_07.py

---
+++ py_func_07.py
@@ -0,0 +1,17 @@
+brojevi = list(range(1, 51))
+# print(numbers)
+def parni(x):
+    return  x % 2 == 0
+
+# parini_brojevi = list(filter(parni, brojevi))
+# print(parini_brojevi)
+# parini_brojevi = list(filter(lambda x: parni(x), brojevi))
+# print(parini_brojevi)
+
+
+parini_brojevi = list(filter(lambda x: x % 2 == 0, brojevi))
+print(parini_brojevi)
+
+
+kvadrati_brojeva = list(map(lambda x: x ** 2, brojevi))
+print(kvadrati_brojeva)
